8c78c10be42f21cb9ebc86b44cc0dcfb198486f6
Harish Krishnoji - 10105547 <harish.krishnoji@fiserv.com>
2021-08-11 22:55:39 +0000

    Dev test

--- ddos_deny_list/denylist.py
+++ ddos_deny_list/denylist.py
@@ -4,102 +4,12 @@ This module contains functions to view, add or delete IP Address
 from SilverLine Denylist.
 """
 
-import requests
 import os
 import yaml
 from helper_fts.email import send_email
 from helper_fts.splunk import splunk_log_event
 from helper_fts.fts_sane import *
-
-requests.packages.urllib3.disable_warnings()
-
-
-def get_denylist(url, token, verify_ssl=False):
-    """Get Tenant specific denylist.
-
-    Args:
-        url (str): SilverLine Portal URL
-        token (str): SilverLine Tenant specific Token
-
-    Returns:
-        Requests response object.
-    """
-    headers = {
-        "Content-Type": "application/json",
-        "X-Authorization-Token": token,
-        "cache-control": "no-cache",
-    }
-    resp = requests.get(url=f"{url}ip_lists/denylist/ip_objects", verify=verify_ssl, headers=headers)
-    return resp.json()
-
-
-def add_ip2denylist(url, token, addr, mask, verify_ssl=False):
-    """Add new IPv4 address to Tenant specific denylist.
-
-    Args:
-        url (str): SilverLine Portal URL
-        token (str): SilverLine Tenant specific Token
-        addr (str): IPv4 Address
-        mask (str): IPv4 network mask
-
-    Returns:
-        int : Requests response status_code
-    """
-    headers = {
-        "Content-Type": "application/json",
-        "X-Authorization-Token": token,
-        "cache-control": "no-cache",
-    }
-    payload = {"data": {"type": "ip_objects", "attributes": {"duration": 0}}}
-    payload["data"]["id"] = addr + "_" + mask
-    payload["data"]["attributes"]["ip"] = addr
-    payload["data"]["attributes"]["mask"] = mask
-
-    resp = requests.post(
-        url=f"{url}ip_lists/denylist/ip_objects?list_target=routed", verify=verify_ssl, headers=headers, json=payload
-    )
-    return resp.status_code
-
-
-def delete_ipfromdenylist(url, token, id, verify_ssl=False):
-    """Delete IPv4 address to Tenant specific denylist.
-
-    Args:
-        url (str): SilverLine Portal URL
-        token (str): SilverLine Tenant specific Token
-        id (str): ID of ip_object which need to be deleted
-
-    Returns:
-        int : Requests response status_code
-    """
-    headers = {
-        "Content-Type": "application/json",
-        "X-Authorization-Token": token,
-        "cache-control": "no-cache",
-    }
-    resp = requests.delete(
-        url=f"{url}ip_lists/denylist/ip_objects/{id}?list_target=routed", verify=verify_ssl, headers=headers
-    )
-    return resp.status_code
-
-
-def view_aciton(url, token):
-    """View data function.
-
-    Args:
-        url (str): SilverLine Portal URL
-        token (str): SilverLine Tenant specific Token
-    """
-    deny_list = []
-    data = get_denylist(url, token)
-    for i in data["data"]:
-        if "routed" in i["attributes"]["list_target"]:
-            deny_list.append(
-                # {"id": i["id"], "ip": i["attributes"]["ip"], "mask": i["attributes"]["mask"], "note": i["meta"]["note"]}
-                {"id": i["id"], "ip": i["attributes"]["ip"], "mask": i["attributes"]["mask"]}
-            )
-    return deny_list
-
+from helperfun import *
 
 if __name__ == "__main__":
 
@@ -113,15 +23,15 @@ if __name__ == "__main__":
     tenant_lst.append(
         {"name": "Fiserv-Development (firstdata7)", "token": os.environ.get("RD_OPTION_TOKEN_ALL").split(",")[0]}
     )
-    tenant_lst.append(
-        {"name": "Fiserv-Clover (firstdatac6)", "token": os.environ.get("RD_OPTION_TOKEN_ALL").split(",")[1]}
-    )
-    tenant_lst.append(
-        {"name": "Fiserv-OFS (firstdatac5)", "token": os.environ.get("RD_OPTION_TOKEN_ALL").split(",")[2]}
-    )
-    tenant_lst.append(
-        {"name": "First Data Corporation (firstdata)", "token": os.environ.get("RD_OPTION_TOKEN_ALL").split(",")[3]}
-    )
+    # tenant_lst.append(
+    #     {"name": "Fiserv-Clover (firstdatac6)", "token": os.environ.get("RD_OPTION_TOKEN_ALL").split(",")[1]}
+    # )
+    # tenant_lst.append(
+    #     {"name": "Fiserv-OFS (firstdatac5)", "token": os.environ.get("RD_OPTION_TOKEN_ALL").split(",")[2]}
+    # )
+    # tenant_lst.append(
+    #     {"name": "First Data Corporation (firstdata)", "token": os.environ.get("RD_OPTION_TOKEN_ALL").split(",")[3]}
+    # )
 
     # Opening JSON file
     f = open("data/deny_list.yml")
